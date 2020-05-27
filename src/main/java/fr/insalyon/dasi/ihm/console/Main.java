8115c9938b2f17f78c6f47ceccb0a5fc6bdf3a3f
Dironiil <berthomet.g@dbmail.com>
2020-05-27 16:58:34 +0200

    peuplerBDD

--- src/main/java/fr/insalyon/dasi/ihm/console/Main.java
+++ src/main/java/fr/insalyon/dasi/ihm/console/Main.java
@@ -22,8 +22,8 @@ public class Main {
 
     public static void main(String[] args) {
         JpaUtil.init();
-//        peuplerBDD();
-        testerHistorique();
+        peuplerBDD();
+//        testerHistorique();
 //        initialiserMediums();
 //        testerConsultations();
 //        testerInscriptionClient();
